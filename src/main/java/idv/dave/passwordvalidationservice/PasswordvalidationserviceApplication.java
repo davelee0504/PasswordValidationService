2d2571b101f7b3939d61c1f0c73a098fcada1350
DaveLeeMac <davelee0504@gmail.com>
2023-07-04 15:04:57 +0800

    Project init
    Project initialization

---
+++ src/main/java/idv/dave/passwordvalidationservice/PasswordvalidationserviceApplication.java
@@ -0,0 +1,13 @@
+package idv.dave.passwordvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordvalidationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordvalidationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/dave/passwordvalidationservice/PasswordvalidationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.dave.passwordvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordvalidationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
